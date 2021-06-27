1e9200879099e8c27adb33c382f3663f728b98bd
Your Name <you@example.com>
2021-06-28 06:35:10 +0700

    sửa axios

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -5,3 +5,9 @@ export const axiosClient = axios.create({
        'content-type': 'application/json'
     }
 })
+export const axiosClientF = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2',
+    headers: {
+       'content-type': 'application/json'
+    }
+})
